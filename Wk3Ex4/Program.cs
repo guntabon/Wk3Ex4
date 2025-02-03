a8d58b90b0276ba364b74b4d11803e862ed2a909
g0364875@amarillocollege.com <g0364875@amarillocollege.com>
2025-02-02 18:10:06 -0600

    Add project files.

---
+++ Wk3Ex4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk3Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Balance = 1000;
+
+            double deposit = 0;
+
+            double withdraw = 0;
+
+            bool notFinished = true;
+
+            int atm = 0;
+
+            while (notFinished)
+            {
+                Console.WriteLine("ATM: Menu: ");
+                Console.WriteLine("1. Check Balance");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Choose an option: ");
+
+                atm = Convert.ToInt32(Console.ReadLine());
+
+                switch (atm)
+                {
+                    case 1:
+                        Console.WriteLine("Your balance is: " + Balance);
+                        continue;
+                    case 2:
+                        Console.WriteLine("Enter Deposit amount: ");
+                        deposit = Convert.ToInt32(Console.ReadLine());
+                        Balance = Balance + deposit;
+                        Console.WriteLine("Deposit Successful");
+                        continue;
+                    case 3:
+                        Console.WriteLine("How much would you like to withdraw?");
+                        withdraw = Convert.ToInt32(Console.ReadLine());
+                        Balance = Balance - withdraw;
+                        Console.WriteLine("WithDrawl successful.");
+                        continue;
+                    case 4:
+                        Console.WriteLine("Goodbye!");
+                        notFinished = false;
+                        break;
+                }
+                
+                Console.ReadLine();
+            }
+        }
+    }
+}
